cdefd96656902b9c01df9e44d2166173323be519
marcelo-machado-efi <marcelo.machado@sejaefi.com.br>
2025-06-24 10:37:52 -0300

    feat: adiciona autopreencher no pix

--- catalog/model/payment/pix/efi_pix_inputs.php
+++ catalog/model/payment/pix/efi_pix_inputs.php
@@ -14,24 +14,29 @@ class EfiPixInputs extends \Opencart\System\Engine\Model
      */
     public function getEntryFormatted(Language $language): array
     {
-        return [
+        // Autopreenchimento para cliente logado
+        $customerName = '';
 
+        if ($this->customer->isLogged()) {
+            $customerName = trim($this->customer->getFirstName() . ' ' . $this->customer->getLastName());
+        }
+
+        return [
             [
-                'name' => 'payment_efi_customer_name',
-                'required' => true,
-                'label' => $language->get('text_label_customer_name'),
-                'type' => 'text',
-                'data-mask' => 'nome'
+                'name'       => 'payment_efi_customer_name',
+                'required'   => true,
+                'label'      => $language->get('text_label_customer_name'),
+                'type'       => 'text',
+                'data-mask'  => 'nome',
+                'value'      => $customerName
             ],
             [
-                'name' => 'payment_efi_customer_document',
-                'required' => true,
-                'label' => $language->get('text_label_customer_document'),
-                'type' => 'text',
-                'data-mask' => 'documento'
-
+                'name'       => 'payment_efi_customer_document',
+                'required'   => true,
+                'label'      => $language->get('text_label_customer_document'),
+                'type'       => 'text',
+                'data-mask'  => 'documento'
             ]
-
         ];
     }
 }
